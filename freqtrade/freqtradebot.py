f7c5d8c7ed97b94ba3d506911e0a0dc073dc96b4
Yazeed Al Oyoun <yazeed.aloyoun@gmail.com>
2020-02-08 21:13:34 +0100

    modified message in modify stoploss scenario

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -756,7 +756,7 @@ class FreqtradeBot:
                 current_rate = self.get_sell_rate(trade.pair, True)
                 if trade.stop_loss > current_rate:
                     logger.info(
-                        f"Changing trailing stop loss for {trade.pair} from {trade.stop_loss} to {current_rate}")
+                        f"Modifying trailing stop loss for {trade.pair} from {trade.stop_loss} to {current_rate}")
                     trade.stop_loss = current_rate
                 try:
                     self.exchange.cancel_order(order['id'], trade.pair)
