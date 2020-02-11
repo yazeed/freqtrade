07321b55d4a79d1bb5490722fca2b4f97bf22b7c
Yazeed Al Oyoun <yazeed.aloyoun@gmail.com>
2020-02-11 22:31:44 +0100

    fixes get_sell_rate and more logging

--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -639,10 +639,10 @@ class FreqtradeBot:
             logger.debug('Using order book to get sell rate')
 
             order_book = self.exchange.get_order_book(pair, 1)
-            rate = order_book['bids'][0][0]
+            rate = order_book['asks'][0][0]
 
         else:
-            rate = self.exchange.fetch_ticker(pair)['bid']
+            rate = self.exchange.fetch_ticker(pair, refresh)['ask']
         self._sell_rate_cache[pair] = rate
         return rate
 
@@ -792,8 +792,8 @@ class FreqtradeBot:
             if (datetime.utcnow() - trade.stoploss_last_update).total_seconds() >= update_beat:
                 # cancelling the current stoploss on exchange first
                 order_id = order['id']
-                current_stop_loss = trade.stop_loss
-                initial_stop_loss = trade.initial_stop_loss
+                current_stop_loss = float("{0:.8f}".format(trade.stop_loss))
+                initial_stop_loss = float("{0:.8f}".format(trade.initial_stop_loss))
                 logger.info(f"Trailing stoploss: cancelling current stoploss on exchange (id:{order_id}) "
                             f"for pair {trade.pair} in order to add another one ...")
                 try:
@@ -805,11 +805,11 @@ class FreqtradeBot:
                 current_sell_rate = self.get_sell_rate(trade.pair, True)
                 current_buy_rate = self.get_buy_rate(trade.pair, True)
                 logger.info(f'Initial trailing stop-loss {initial_stop_loss} vs '
+                            f'Trade.stop_loss trailing stop-loss {trade.stop_loss} vs '
                             f'Current trailing stop-loss {current_stop_loss} vs '
-                            f'Target trailing stop-loss {trade.stop_loss} vs '
                             f'Do-able sell rate {current_sell_rate} vs '
                             f'Do-able buy rate {current_buy_rate}')
-                if trade.stop_loss > current_buy_rate:
+                if trade.stop_loss >= current_sell_rate:
                     logger.info(
                         f"Moving target trailing stop loss for {trade.pair} "
                         f"from target {trade.stop_loss} to do-able {current_buy_rate}")
