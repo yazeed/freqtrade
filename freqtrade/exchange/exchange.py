a5e89751c263a939045063fdbe83be29a9378958
Yazeed Al Oyoun <yazeed.aloyoun@gmail.com>
2020-02-14 23:36:14 +0100

    all my fixes

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -490,6 +490,7 @@ class Exchange:
                      rate: float, params: Dict = {}) -> Dict:
         logger.info(f"create_order({pair},{ordertype},{side},{amount},{rate},{params})")
         try:
+            logger.info(f"create_order({pair},{ordertype},{side},{amount},{rate},{params})2")
             # Set the precision for amount and price(rate) as accepted by the exchange
             amount = self.amount_to_precision(pair, amount)
             needs_price = (ordertype != 'market'
--- freqtrade/freqtradebot.py
+++ freqtrade/freqtradebot.py
@@ -730,8 +730,7 @@ class FreqtradeBot:
             trade.stoploss_order_id = None
             logger.error(f'Unable to place a stoploss order on exchange. {e}')
             logger.warning('Selling the trade forcefully')
-            # trade.stop_loss
-            self.execute_sell(trade, stop_price, sell_reason=SellType.EMERGENCY_SELL)
+            self.execute_sell(trade, trade.stop_loss, sell_reason=SellType.EMERGENCY_SELL)
             return False
         except DependencyException:
             trade.stoploss_order_id = None
