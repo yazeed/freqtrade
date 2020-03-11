21eae8aa7427a7461420a09fc4ab963be72e06fb
Yazeed Al Oyoun <yazeed.aloyoun@gmail.com>
2020-03-11 17:19:23 +0100

    only buy on new candles

--- freqtrade/strategy/interface.py
+++ freqtrade/strategy/interface.py
@@ -276,6 +276,14 @@ class IStrategy(ABC):
 
         latest = dataframe.iloc[-1]
 
+        # Check if dataframe has new candle - act only within 30 mins
+        signal_date = arrow.get(latest['date'])
+        interval_minutes = timeframe_to_minutes(interval)
+        if (arrow.utcnow() - signal_date).total_seconds() // 60 >= interval_minutes / 8:
+            logger.warning('Old candle for pair %s. Last candle is %s minutes old',
+                           pair, int((arrow.utcnow() - signal_date).total_seconds() // 60))
+            return False, False
+
         # Check if dataframe is out of date
         signal_date = arrow.get(latest['date'])
         interval_minutes = timeframe_to_minutes(interval)
